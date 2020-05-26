af38e17cbf93e1a6b3a04921f95833e9929b000a
Venka1995 <Venkateshaks598@gmail.com>
2020-05-26 22:22:08 +0530

    commit after adding bootstrap(not full) for todo

--- list/forms.py
+++ list/forms.py
@@ -3,6 +3,7 @@ from .models import Todo
 
 
 class ListForm(forms.ModelForm):
+    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Add new task...', 'class': 'form-control'}))
     class Meta:
         model = Todo
         fields = '__all__'
